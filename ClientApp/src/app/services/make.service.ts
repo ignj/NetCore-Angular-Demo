70d52640ad5551e2c826428ce4d43e6ef45557d6
ignacio jocano <ignacio.jocano@gmail.com>
2019-05-19 11:24:17 -0300

    Added makes to dropdown new page

--- ClientApp/src/app/services/make.service.ts
+++ ClientApp/src/app/services/make.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class MakeService {
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   getMakes(){
-    return this.http.get('/api/makes')
-      .map(res => res.json());
+    return this.http.get('/api/makes');      
   }
 }
