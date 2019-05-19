0dcf8b603978d3ffe20eeb32725b692b219f24cf
ignacio jocano <ignacio.jocano@gmail.com>
2019-05-19 18:12:57 -0300

    Added vehicle model and simple post endpoint to test

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCore_Angular_Demo.Models;
+
+namespace NetCore_Angular_Demo.Controllers
+{
+    [Route("/api/vehicles")]
+    public class VehiclesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("ok");
+        }
+
+        [HttpPost]
+        public IActionResult CreateVehicle([FromBody] Vehicle vehicle)
+        {
+            return Ok(vehicle);
+        }
+    }
+}
--- Persistence/AppDbContext.cs
+++ Persistence/AppDbContext.cs
@@ -5,12 +5,23 @@ namespace NetCore_Angular_Demo.Persistence
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-
-        }
-
         public DbSet<Make> Makes { get; set; }
         public DbSet<Feature> Features { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf =>
+            new
+            {
+                vf.VehicleId,
+                vf.FeatureId
+            });
+        }
     }
 }
