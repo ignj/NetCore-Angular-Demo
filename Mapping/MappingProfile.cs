3c8920281a984d7b9c74ca51a9ef21f65b2b67e2
ignacio jocano <ignacio.jocano@gmail.com>
2019-05-18 21:14:54 -0300

    Added Makes controller and mapper

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using NetCore_Angular_Demo.Controllers.Resources;
+using NetCore_Angular_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore_Angular_Demo.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Make, MakeResource>();
+            CreateMap<Model, ModelResource>();
+        }
+    }
+}
