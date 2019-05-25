0d47fe3cce2749eb504c696866c1ca5e12ba7304
ignacio jocano <ignacio.jocano@gmail.com>
2019-05-25 11:57:10 -0300

    Added makes repository, and updated makes controller

---
+++ Core/IMakesRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore_Angular_Demo.Core
+{
+    public interface IMakesRepository
+    {
+        Task<IEnumerable<Make>> GetMakes();
+
+        Task<IEnumerable<Make>> GetMakesWithModels();
+    }
+}
---
+++ Persistence/MakesRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCore_Angular_Demo.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore_Angular_Demo.Persistence
+{
+    public class MakesRepository : IMakesRepository
+    {
+        private readonly AppDbContext context;
+
+        public MakesRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IEnumerable<Make>> GetMakes()
+        {
+            return await context.Makes.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Make>> GetMakesWithModels()
+        {
+            return await context.Makes.Include(m => m.Models).ToListAsync();
+        }
+    }
+}
